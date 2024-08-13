93e8171da284522279a9f300694a297d92567f8d
farid <farid@LAPTOP-SPTVSQVM>
2024-08-13 20:00:00 +0530

    Add project files.

---
+++ Assignment 2/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+class Assignment2
+{
+    static void Main(string[] args)
+    {
+        float a,b;
+        Console.WriteLine("enter two floating point nums:");
+        a = float.Parse(Console.ReadLine());
+        b = float.Parse(Console.ReadLine());
+        Console.WriteLine($"{a} * {b} = {a * b}");
+    }
+}
